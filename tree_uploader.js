00b39e798c83c47a4af36483f979a8f7a2e2bf49
samsucik <s.sucik@rasa.com>
2022-08-22 15:52:54 +0200

    Create a tiny JS script that enters trees from JSON into the fruitmap system

---
+++ tree_uploader.js
@@ -0,0 +1,16 @@
+data_to_upload = []  // replace [] with your data
+
+options_data = {}
+$("#sel option").each(function(idx){
+    if (idx != 0) {
+        options_data[$(this).attr("label")] = $(this).val();
+    }
+});
+
+data_to_upload.forEach(function(tree, idx){
+    console.log((idx+1) + ". tree: " + tree.name + " (" + tree.lat + ", " + tree.lon + ")");
+    tree_id = options_data[tree.name];
+    $("#pozicie").append('<input type="hidden" id="tree' + (idx+1) + '" name="tree[' + (idx+1) + ']" value="' + tree.lat + "," + tree.lon + "," + tree_id + '" />');
+});
+
+console.log("All trees have been entered even though you see no markers on the map. Press 'Uložiť' to save all added trees.")
