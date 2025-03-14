3769cd1b888ec5ea7878c4271fe423056732278d
samsucik <ssucik@cisco.com>
2025-03-14 09:44:12 +0100

    Make an artificial dummy change

--- tree_uploader.js
+++ tree_uploader.js
@@ -13,4 +13,4 @@ data_to_upload.forEach(function(tree, idx){
     $("#pozicie").append('<input type="hidden" id="tree' + (idx+1) + '" name="tree[' + (idx+1) + ']" value="' + tree.lat + "," + tree.lon + "," + tree_id + '" />');
 });
 
-console.log("All trees have been entered even though you see no markers on the map. Press 'Save'/'Uložiť' to save all added trees.")
+console.log("All trees have been entered even though you see no markers on the map. Press 'Save'/'Uložiť' to save all added trees!")
